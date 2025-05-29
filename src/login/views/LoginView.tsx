d2e0160c6ffc0798c8dd4064bdfd7310be3643cd
alanfg120 <alanfg120@gmail.com>
2025-05-29 15:47:10 -0500

    init

--- src/login/views/LoginView.tsx
+++ src/login/views/LoginView.tsx
@@ -1,4 +1,5 @@
+import style from "./../css/login.module.css";
 
 export default function LoginView() {
-     return  <h1>Hola soy Login</h1>
+  return <h1 className={style.button}>Hola soy Login</h1>;
 }
