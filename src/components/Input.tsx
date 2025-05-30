d2dfab055e00b379b1762c15f30a513225abd879
gerste1023 <gerstegomez1@hotmail.com>
2025-05-30 15:57:39 -0500

    add card component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,20 +1,21 @@
 import { Field, Input } from "@chakra-ui/react"
 
-interface Propiedades {
+
+interface Props {
     invalid: boolean;
     label: string;
     placeholder: string;
     errorText?: string;
 }
 
-function InputText({ invalid,label,placeholder,errorText }: Propiedades ) {
-    return ( <>
-        <Field.Root invalid={invalid}>
+function InputText({ invalid,label,placeholder,errorText }: Props ) {
+    return ( 
+        <Field.Root className="mb-1" invalid={invalid}>
         <Field.Label>{label}</Field.Label>
         <Input placeholder={placeholder} />
         <Field.ErrorText>{errorText}</Field.ErrorText>
         </Field.Root>
-    </>);
+    );
 }
 
 export default InputText;
--- src/login/views/LoginView.tsx
+++ src/login/views/LoginView.tsx
@@ -1,64 +1,36 @@
 import InputText from "../../components/Input";
-import { Button, Card, Field, Stack } from "@chakra-ui/react"
+import { Button } from "@chakra-ui/react"
 import './../../index.css';
 import './../css/login.module.css'
+import CardChakra from '../../components/Card';
 
 export default function LoginView() {
   return (
     <>
-      {/* <div className="justify-content-c align-content-c">
-      <Card.Root className="" maxW="sm">
-        <Card.Header>
-          <Card.Title>Sign up</Card.Title>
-          <Card.Description>
-            Fill in the form below to create an account
-          </Card.Description>
-        </Card.Header>
-        <Card.Body>
-          <Stack gap="4" w="full">
-            <Field.Root>
-              <InputText
-                invalid={false}
-                label={"User Name"}
-                placeholder={"UserName"}
-                errorText={"UserName is Invalid"}
-              />
-            </Field.Root>
-            <Field.Root>
-              <InputText
-                invalid={false}
-                label={"Password"}
-                placeholder={"Password"}
-                errorText={"Password is Invalid"}
-              />
-            </Field.Root>
-          </Stack>
-        </Card.Body>
-        <Card.Footer justifyContent="flex-end">
-          <Button variant="outline">Cancel</Button>
-          <Button variant="solid">Sign in</Button>
-        </Card.Footer>
-      </Card.Root>
-    </div> */}
-
-    <div className="h-100 justify-content-c align-items-c">
-      <div>
-        <h2 className="mb-1">Iniciar sesión</h2>
-         <InputText
-            invalid={false}
-            label={"Usuario"}
-            placeholder={"Usuario"}
-            errorText={"Usuario Invalido"}
-          />
-          <InputText
-            invalid={false}
-            label={"Contraseña"}
-            placeholder={"Contraseña"}
-            errorText={"Contraseña Invalido"}
-          />
-          <Button className="w-100 mt-1" variant="solid">Sign in</Button>
+   <div className="h-100 justify-content-c align-items-c cardWidth">
+        <CardChakra 
+          title="Iniciar Sesion"
+          description="Ingrese sus credenciales para acceder"
+        >
+          <div className="login-form-content">
+            <InputText
+              invalid={false}
+              label={"Usuario"}
+              placeholder={"Ingrese su usuario"}
+              errorText={"Usuario Invalido"}
+            />
+            <InputText
+              invalid={false}
+              label={"Contraseña"}
+              placeholder={"Ingrese su contraseña"}
+              errorText={"Contraseña Invalida"}
+            />
+            <Button className="w-100 mt-1" variant="solid">
+              Sign in
+            </Button>
+          </div>
+        </CardChakra>
       </div>
-    </div>
     </>
   );
 }
